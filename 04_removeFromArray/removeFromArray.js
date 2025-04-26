ae2d269d917fe21616f3039c5435506544091729
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-04-26 03:50:37 -0700

    Complete first few tests for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (passedArray, unwantedNum) {
+  const newArray = [];
+
+  for (let i = 0; i < passedArray.length; i++) {
+    if (passedArray[i] === unwantedNum) {
+      continue;
+    }
+
+    newArray.push(passedArray[i]);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
