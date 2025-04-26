4422b8ffe95d0b2387d6b05ed2c661257138eef1
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-04-26 15:03:46 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(num1)|| !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else {
+        let biggerNum;
+        let smallerNum;
 
+        if (num1 > num2) {
+            biggerNum = num1;
+            smallerNum = num2;
+        }
+        else {
+            biggerNum = num2
+            smallerNum = num1;
+        }
+
+        let result = 0;
+        for (let i = smallerNum; i <= biggerNum; i++) {
+            result += i;
+        }
+
+        return result;
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log("result: " + sumAll(2, 4));
