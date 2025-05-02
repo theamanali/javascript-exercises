2408d07ecc42980fbf9326ff4e2c11f36c3ab7dd
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-05-02 16:26:54 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+    let stringArray = string.toLowerCase().split('');
+    stringArray = stringArray.filter(char => alphanumerical.includes(char))
+    string = stringArray.join('');
+
+    let endIndex = string.length - 1;
+
+    for (let i = 0; i < string.length; i++, endIndex--) {
+        if (string[i] !== string[endIndex]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
