502510aad0579ac5452a6e8e4e2456c29d4df39b
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-05-02 16:45:09 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) return "OOPS";
 
+    if (num === 0) return 0;
+    else if (num === 1) return 1;
+    else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 };
 
 // Do not edit below this line
