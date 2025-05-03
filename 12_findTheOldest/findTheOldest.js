c7dcade08a843e077dd15818befbac6533a23a01
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-05-02 17:18:19 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getAge(person) {
+    let death = person.yearOfDeath;
 
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
+
+        if (oldestAge < currentAge) {
+            return current;
+        }
+        else return oldest;
+    });
 };
 
 // Do not edit below this line
